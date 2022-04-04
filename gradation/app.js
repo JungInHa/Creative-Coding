11af07ff5fc6bd89e42add9eccddfe78de82b626
JungInHa <juudyy99@gmail.com>
2022-04-04 20:39:48 +0900

    💄 Declare color constants

---
+++ gradation/app.js
@@ -0,0 +1,7 @@
+const COLORS = [
+  {r: 45, g: 74, b: 227}, // blue
+	{r: 250, g: 255, b: 89}, // yellow
+  {r: 255, g: 104, b: 248}, // purple
+  {r: 44, g: 209, b: 252}, // skyblue
+  {r: 54, g: 74233, b: 84}, // green
+]
